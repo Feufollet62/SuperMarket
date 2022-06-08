7c50d41ecf3fd976aa79076e370831618fbd0716
Eliot <fromenteliot@gmail.com>
2022-06-08 12:20:26 +0200

    Dash basique sans knockback / stun Feedback de mouvement avec "animation" du joueur + particules

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,22 +13,30 @@ public class PlayerController : MonoBehaviour
     [Header("Mouvement")]
     [SerializeField] private float maxAccel = 30f;
     [SerializeField] private float maxSpeed = 8f;
+    [SerializeField] private float dashStrength = 1f;
+    [SerializeField] private float dashLength = .5f;
 
     [Header("Animation")]
     [SerializeField] private float animIntensity = 2f;
     [SerializeField] private float animMaxAngle = 20f;
     
     // Private
-    private Rigidbody rb;
-    private Vector3 velocity, desiredVelocity;
+    private Rigidbody _rb;
+    private ParticleSystem _particleSystem;
     
-    private Vector3 inputMovement;
-    private bool inputDash, inputInteract;
+    private Vector3 _velocity, _desiredVelocity, _inputMovement;
+
+    private bool _dashing;
+    private float _dashTimer;
+    
+    private bool _inputDash, _inputInteract;
+
+    private float _animTimer;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        PlayerManager manager = PlayerManager.Instance;
+        _rb = GetComponent<Rigidbody>();
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
         
         // Assigne les bons matériaux aux bons joueurs
         GetComponentInChildren<MeshRenderer>().material = playerMats[id];
@@ -39,60 +46,101 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         Animate();
+        print("Dashing: " + _dashing);
+        print("Timer: " + _dashTimer);
     }
 
     private void FixedUpdate()
     {
         Move();
+        Dash();
     }
 
     // https://youtu.be/5tOOstXaIKE
     public void OnMovement(InputAction.CallbackContext context)
     {
         Vector2 rawInput = context.ReadValue<Vector2>();
-        inputMovement = new Vector3(rawInput.x, 0, rawInput.y);
+        _inputMovement = new Vector3(rawInput.x, 0, rawInput.y);
     }
 
     public void OnDash(InputAction.CallbackContext context)
     {
-        //inputDash = context.ReadValue<bool>();
-        inputDash = context.action.triggered;
+        _inputDash = context.action.triggered;
     }
     
     public void OnInteract(InputAction.CallbackContext context)
     {
         //inputInteract = context.ReadValue<bool>();
-        inputInteract = context.action.triggered;
+        _inputInteract = context.action.triggered;
     }
 
     private void GetInput()
     {
-        desiredVelocity = inputMovement * maxSpeed;
+        _desiredVelocity = _inputMovement * maxSpeed;
     }
     
     private void Move()
     {
+        if(_dashing) return;
+        
+        print("Moving");
+        
         float maxSpeedChange = maxAccel * Time.deltaTime;
         
-        velocity = rb.velocity;
+        _velocity = _rb.velocity;
         
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, maxSpeedChange);
+        _velocity.z = Mathf.MoveTowards(_velocity.z, _desiredVelocity.z, maxSpeedChange);
 
         //transform.position += velocity * Time.deltaTime;
-        rb.velocity = velocity;
+        _rb.velocity = _velocity;
+    }
+
+    private void Dash()
+    {
+        // Already dashing
+        if(_dashing)
+        {
+            print("Currently dashing " + Time.time);
+            _dashTimer += Time.fixedDeltaTime;
+            if (_dashTimer >= dashLength)
+            {
+                _dashing = false;
+            }
+            return;
+        }
+        
+        // No input
+        if (!_inputDash) return;
+        
+        // Start dashing
+        _inputDash = false;
+        _dashing = true;
+        _dashTimer = 0f;
+        
+        print("Start dash " + Time.time);
+        _rb.AddForce(_inputMovement * dashStrength, ForceMode.Impulse);
     }
 
     private void Animate()
     {
-        if(inputMovement == Vector3.zero)
+        if(_inputMovement == Vector3.zero)
         {
             model.Rotate(0,0,0);
+            _particleSystem.Stop();
+            _animTimer = 0;
             return;
+        }
+
+        _animTimer += Time.deltaTime;
+        if (_animTimer > .5f)
+        {
+            _particleSystem.Play();
+            _animTimer = 0f;
         }
         
         // On retrouve l'accélération
-        Vector3 accel = inputMovement*maxSpeed - velocity;
+        Vector3 accel = _inputMovement*maxSpeed - _rb.velocity;
 
         model.forward = accel;
         float angle = accel.magnitude * animIntensity;
