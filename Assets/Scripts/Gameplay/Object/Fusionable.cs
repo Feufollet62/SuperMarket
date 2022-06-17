42dfa1fbe119b517ca3eb7de623131491b515583
Zoreol <rlcorentin59974@gmail.com>
2022-06-17 10:35:35 +0200

    Script atelier comment

--- Assets/Scripts/Gameplay/Object/Fusionable.cs
+++ Assets/Scripts/Gameplay/Object/Fusionable.cs
@@ -4,122 +4,113 @@ using UnityEngine;
 
 public class Fusionable : MonoBehaviour
 {
+    #region Variables
+
+    //liste possible de fusion (scritableObject)
     [SerializeField] FusionData[] fusions;
+    //liste de position (plus pratique pour l'anim et pour rechopper l'objet suplementaire)
     [SerializeField] Transform posObjet1;
     [SerializeField] Transform posObjet2;
     [SerializeField] Transform posRejet;
+    //temps de conception
     [SerializeField] float timeToReceip;
+    //Prend les objet poser permettant de regarder son id
     [SerializeField] List<GameObject> objectOnLabo;
 
+    //condition pour :
+    //savoir si la premiere place est prise
     private bool pos1 = false;
+    //si l'objet est en construction
     private bool useFabric = false;
-    private bool canFuse = true;
+    //verif si mes objet son dans ma collection
+    private bool item1Verif = false;
+    private bool item2Verif = false;
 
+    //creation d'objet pour l'anim (et l'appeler dans d'autre fonction)
     private GameObject Item1;
     private GameObject Item2;
     private GameObject newItem;
 
+    #endregion
+
+    #region Custom Function
+
     private void OnTriggerEnter(Collider other)
     {
+        //si mon objet est de type objet 
+        //que la fabrication n'est pas en cours
+        //et si les objet sont craftable
         if (other.gameObject.CompareTag("Object") && !useFabric && other.gameObject.GetComponent<ObjectDefinition>().dataObject.craftable)
         {
+            //si ma 1ere place n'est pas prise
             if (!pos1)
             {
-                objectOnLabo.Add(other.gameObject);
-                Destroy(other.gameObject);
-                Debug.Log("Pos1Prise");
-                Item1 = Instantiate(objectOnLabo[0], posObjet1.position, posObjet1.rotation);
+                //ma place une est prise
                 pos1 = true;
+                //j'ajoute un objet dans ma liste
+                objectOnLabo.Add(other.gameObject);
+                //destruction de ma liste
+                Destroy(other.gameObject);
+                //creation d'un objet pour animé 
+                Item1 = Instantiate(objectOnLabo[0], posObjet1.position, posObjet1.rotation);
             }
             if (pos1)
             {
+                //j'ajoute un objet dans ma liste
                 objectOnLabo.Add(other.gameObject);
+                //destruction de ma liste
                 Destroy(other.gameObject);
-                Debug.Log("Pos2Prise");
+                //creation d'un objet pour animé 
                 Item2 = Instantiate(objectOnLabo[1], posObjet2.position, posObjet2.rotation);
+                //on se met en fabrication
                 useFabric = true;
+                //lancement de la fabrique
                 StartCoroutine(FusionObjet());
             }
-        }
-    }
-    void Verification()
-    {
-        if (useFabric)
-        {
-            for (int i = 0; i < fusions.Length; i++)
-            {
-                if (Item1.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion1.iD ||
-                    Item1.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion2.iD)
-                {
-                    Debug.Log("Premiere Verif Faite");
-                    Debug.Log(Item1.GetComponent<ObjectDefinition>().dataObject.iD);
-                    
-                    Debug.Log(fusions[i].objetFusion1.iD);
-                }
-                if (Item2.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion1.iD || 
-                    Item2.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion2.iD)
-                {
-                    Debug.Log("Second Verif Faite");
-                    Debug.Log(Item2.GetComponent<ObjectDefinition>().dataObject.iD);
-                }/*
-                if (Item1.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion1.iD ||
-                    Item1.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion2.iD ||
-                    Item2.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion1.iD ||
-                    Item2.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion2.iD)
-                {
-                    canFuse = false;
-                }*/
-            }
-            StartCoroutine(FusionObjet());
         }
     }
 
     IEnumerator FusionObjet()
     {
-
+        //temps de fabrique
         yield return new WaitForSeconds(timeToReceip);
 
         if (useFabric)
         {
             for (int i = 0; i < fusions.Length; i++)
             {
+                //connaitre mon premier objet que je met
                 if (Item1.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion1.iD ||
                     Item1.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion2.iD)
                 {
-                    Debug.Log("Premiere Verif Faite");
-                    Debug.Log(Item1.GetComponent<ObjectDefinition>().dataObject.iD);
-
-                    Debug.Log(fusions[i].objetFusion1.iD);
+                    //mon item est bon
+                    item1Verif = true;
                 }
+                //connaitre mon deuxieme objet que je met
                 if (Item2.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion1.iD ||
                     Item2.GetComponent<ObjectDefinition>().dataObject.iD == fusions[i].objetFusion2.iD)
                 {
-                    Debug.Log("Second Verif Faite");
-                    Debug.Log(Item2.GetComponent<ObjectDefinition>().dataObject.iD);
+                    //mon second item est bon
+                    item2Verif = true;
                     newItem = Instantiate(fusions[i].objetResult, posRejet.position, posRejet.rotation);
-                }/*
-                if (Item1.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion1.iD ||
-                    Item1.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion2.iD ||
-                    Item2.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion1.iD ||
-                    Item2.GetComponent<ObjectDefinition>().dataObject.iD != fusions[i].objetFusion2.iD)
+                }
+                //pour plus tard pour des objet complexe comme deux objet different
+                /*
+                //si mes deux item sont bon
+                if (item1Verif && item2Verif)
                 {
-                    canFuse = false;
+                    //crée l'objet
+                    newItem = Instantiate(fusions[i].objetResult, posRejet.position, posRejet.rotation);
                 }*/
             }
-            //StartCoroutine(FusionObjet());
-
-
+            //elever tout ce qui as sur mon atelier
             Destroy(Item1);
             Destroy(Item2);
             objectOnLabo.Clear();
         }
-
-        //if (canFuse)
-        //{
-            //newItem = Instantiate(fusions[0].objetResult, posRejet.position, posRejet.rotation);
-        //}
-
+        //dire que c'est vide
         pos1 = false;
         useFabric = false;
     }
+    #endregion
 }
